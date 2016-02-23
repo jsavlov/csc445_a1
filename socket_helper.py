6e9e795140f9a2270b9f4273252b327eb44efc7f
Jason Savlov <jsavlov@oswego.edu>
2016-02-23 14:28:29 -0500

    Added an optional isUDP parameter to fresh_socket() method

--- socket_helper.py
+++ socket_helper.py
@@ -1,7 +1,11 @@
 import socket
 
-def fresh_socket(host, port):
-    f_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # The socket object used to connect to the server
+def fresh_socket(host, port, isUDP=False):
+    # The socket object used to connect to the server.
+    # If the isUDP parameter of the function is true, socket.SOCK_DGRAM is used.
+    # Otherwise, it uses socket.SOCK_STREAM.
+    # For more info, see http://www.binarytides.com/programming-udp-sockets-in-python/
+    f_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM if isUDP else socket.SOCK_STREAM)
 
     print "Opening connection to host " + str(host) + " on port " + str(port) + "..."
 
